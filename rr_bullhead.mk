bf13ab54c3fd17a87ddf68f491ba0a208428de73
winebar <winebar@protonmail.com>
2019-10-30 00:10:02 -0400

    RR-ify attempt

--- rr_bullhead.mk
+++ rr_bullhead.mk
@@ -2,20 +2,20 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common LineageOS stuff.
+# Inherit some common RR stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_bullhead
+PRODUCT_NAME := RR_bullhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5X
 TARGET_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM7.181005.003 4984324 release-keys"
+    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM7.181205.001 5080180 release-keys"
 
-BUILD_FINGERPRINT := google/bullhead/bullhead:8.1.0/OPM7.181005.003/4984324:user/release-keys
+BUILD_FINGERPRINT := google/bullhead/bullhead:8.1.0/OPM7.181205.001/5080180:user/release-keys
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 #
-# Copyright 2015 The Android Open Source Project
+# Copyright 2013 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,12 +14,4 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_bullhead.mk \
-    $(LOCAL_DIR)/aosp_bullhead_svelte.mk \
-    $(LOCAL_DIR)/rr_bullhead.mk
-
-COMMON_LUNCH_CHOICES := \
-    aosp_bullhead-userdebug \
-    aosp_bullhead_svelte-userdebug \
-    lineage_bullhead-userdebug
+add_lunch_combo aosp_bullhead-userdebug
